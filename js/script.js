d2f04aaeb4ab5e0bbfe7b4e33a5040c420c098b1
mohelalfy <mohelalfy77@gmail.com>
2025-03-16 21:12:57 +0200

    feat: Add hover effect
    Changes color of grid item on hover

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ const containerDiv = document.querySelector("#grid");
 
 for (let i = 0; i < 16; i++) {
   const div = document.createElement("div");
-  div.className = "grid-item";
+  div.classList.add("grid-item");
+
+  div.addEventListener("mouseenter", (e) => {
+    div.classList.add("grid-item-hovered");
+  });
 
   containerDiv.append(div);
 }
