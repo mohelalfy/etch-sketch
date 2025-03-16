5ef953cd0a70aea8bd7c30faf2a1226834cf7c7c
mohelalfy <mohelalfy77@gmail.com>
2025-03-16 21:27:57 +0200

    feat: Add Reset button
    Resets grid and creates new one with new size the user chose

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 const containerDiv = document.querySelector("#grid");
+const resetButton = document.querySelector("#reset-button");
 
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  div.classList.add("grid-item");
-
-  div.addEventListener("mouseenter", (e) => {
-    div.classList.add("grid-item-hovered");
-  });
-
-  containerDiv.append(div);
+function resetGrid() {
+  containerDiv.replaceChildren();
 }
+
+function generateGrid(length) {
+  for (let i = 0; i < length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid-item");
+
+    div.addEventListener("mouseenter", (e) => {
+      div.classList.add("grid-item-hovered");
+    });
+
+    containerDiv.append(div);
+  }
+}
+
+resetButton.addEventListener("click", () => {
+  const length = parseInt(prompt("Enter new grid size"));
+
+  if (length > 100) {
+    alert("Max length is 100, try again with smaller number");
+    return;
+  }
+
+  resetGrid();
+  generateGrid(length * length);
+});
+
+generateGrid(16);
